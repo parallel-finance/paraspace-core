218d301892619ff7b04a742ec43aa8066b587fc0
zhoujia6139 <zhoujia6139@126.com>
2022-09-14 23:15:22 +0800

    feat: add ETH support for Increase and Decrease Liquidity by NTokenUn… (#12)
    * feat: add ETH support for Increase and Decrease Liquidity by NTokenUniswapV3
    
    * fix: typos
    
    * fix: code style
    
    * refactor: code style

--- contracts/protocol/tokenization/NTokenUniswapV3.sol
+++ contracts/protocol/tokenization/NTokenUniswapV3.sol
@@ -14,6 +14,7 @@ import {IPool} from "../../interfaces/IPool.sol";
 import {NToken} from "./NToken.sol";
 import {DataTypes} from "../libraries/types/DataTypes.sol";
 import {INonfungiblePositionManager} from "../../dependencies/uniswap/INonfungiblePositionManager.sol";
+import {IWETH} from "../../misc/interfaces/IWETH.sol";
 
 /**
  * @title UniswapV3 NToken
@@ -87,7 +88,8 @@ contract NTokenUniswapV3 is NToken {
         uint256 tokenId,
         uint128 liquidityDecrease,
         uint256 amount0Min,
-        uint256 amount1Min
+        uint256 amount1Min,
+        bool receiveEthAsWeth
     ) internal returns (uint256 amount0, uint256 amount1) {
         if (liquidityDecrease > 0) {
             // amount0Min and amount1Min are price slippage checks
@@ -107,29 +109,64 @@ contract NTokenUniswapV3 is NToken {
             );
         }
 
+        (
+            ,
+            ,
+            address token0,
+            address token1,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+
+        ) = INonfungiblePositionManager(_underlyingAsset).positions(tokenId);
+
+        address weth = _addressesProvider.getWETH();
+        receiveEthAsWeth = (receiveEthAsWeth &&
+            (token0 == weth || token1 == weth));
+
+        address sender = msg.sender;
+        uint128 MAX_INT_128 = 0xffffffffffffffffffffffffffffffff;
         INonfungiblePositionManager.CollectParams
             memory collectParams = INonfungiblePositionManager.CollectParams({
                 tokenId: tokenId,
-                recipient: _msgSender(),
-                amount0Max: 2**128 - 1,
-                amount1Max: 2**128 - 1
+                recipient: receiveEthAsWeth ? address(this) : sender,
+                amount0Max: MAX_INT_128,
+                amount1Max: MAX_INT_128
             });
 
         (amount0, amount1) = INonfungiblePositionManager(_underlyingAsset)
             .collect(collectParams);
+
+        require(amount0 >= amount0Min, "Insufficient amount0");
+        require(amount1 >= amount1Min, "Insufficient amount1");
+
+        if (receiveEthAsWeth) {
+            uint256 balanceWeth = IERC20(weth).balanceOf(address(this));
+            if (balanceWeth > 0) {
+                IWETH(weth).withdraw(balanceWeth);
+                _safeTransferETH(sender, balanceWeth);
+            }
+
+            address pairToken = (token0 == weth) ? token1 : token0;
+            uint256 balanceToken = IERC20(pairToken).balanceOf(address(this));
+            if (balanceToken > 0) {
+                IERC20(pairToken).safeTransfer(sender, balanceToken);
+            }
+        }
     }
 
     /**
      * @notice Increases liquidity in the current range
      * @dev Pool must be initialized already to add liquidity
      * @param tokenId The id of the erc721 token
-     * @param amount0 The amount to add of token0
-     * @param amount1 The amount to add of token1
+     * @param amountAdd0 The amount to add of token0
+     * @param amountAdd1 The amount to add of token1
      * @param amount0Min The minimum amount to add of token0
      * @param amount1Min The minimum amount to add of token1
-     * @return liquidity The amount of liquidity added
-     * @return amount0 The amount of token0 added as liquidity
-     * @return amount1 The amount of token1 added as liquidity
      */
     function _increaseLiquidityCurrentRange(
         uint256 tokenId,
@@ -137,14 +174,7 @@ contract NTokenUniswapV3 is NToken {
         uint256 amountAdd1,
         uint256 amount0Min,
         uint256 amount1Min
-    )
-        internal
-        returns (
-            uint128 liquidity,
-            uint256 amount0,
-            uint256 amount1
-        )
-    {
+    ) internal {
         (
             ,
             ,
@@ -161,32 +191,20 @@ contract NTokenUniswapV3 is NToken {
         ) = INonfungiblePositionManager(_underlyingAsset).positions(tokenId);
 
         // move underlying into this contract
-        IERC20(token0).safeTransferFrom(
-            _msgSender(),
-            address(this),
-            amountAdd0
-        );
-        IERC20(token1).safeTransferFrom(
-            _msgSender(),
-            address(this),
-            amountAdd1
-        );
-
-        uint256 token0Allownance = IERC20(token0).allowance(
-            address(this),
-            _underlyingAsset
-        );
-        if (token0Allownance == 0) {
-            IERC20(token0).safeApprove(_underlyingAsset, 2**256 - 1);
+        address weth = _addressesProvider.getWETH();
+        uint256 txValue = msg.value;
+        address sender = msg.sender;
+        bool token0IsETH = (token0 == weth && txValue > 0);
+        bool token1IsETH = (token1 == weth && txValue > 0);
+        if (!token0IsETH) {
+            IERC20(token0).safeTransferFrom(sender, address(this), amountAdd0);
+        }
+        if (!token1IsETH) {
+            IERC20(token1).safeTransferFrom(sender, address(this), amountAdd1);
         }
 
-        uint256 token1Allownance = IERC20(token1).allowance(
-            address(this),
-            _underlyingAsset
-        );
-        if (token1Allownance == 0) {
-            IERC20(token1).safeApprove(_underlyingAsset, 2**256 - 1);
-        }
+        checkAllownance(token0);
+        checkAllownance(token1);
 
         // move underlying from this contract to Uniswap
         INonfungiblePositionManager.IncreaseLiquidityParams
@@ -201,19 +219,28 @@ contract NTokenUniswapV3 is NToken {
                 });
 
         // return information about amount increased
-        (liquidity, amount0, amount1) = INonfungiblePositionManager(
+        (, uint256 amount0, uint256 amount1) = INonfungiblePositionManager(
             _underlyingAsset
-        ).increaseLiquidity(params);
+        ).increaseLiquidity{value: txValue}(params);
 
         // refund unused tokens
-        if (amount0 < amountAdd0) {
+        if (amount0 < amountAdd0 && !token0IsETH) {
             uint256 refund0 = amountAdd0 - amount0;
-            IERC20(token0).safeTransfer(_msgSender(), refund0);
+            IERC20(token0).safeTransfer(sender, refund0);
         }
 
-        if (amount1 < amountAdd1) {
+        if (amount1 < amountAdd1 && !token1IsETH) {
             uint256 refund1 = amountAdd1 - amount1;
-            IERC20(token1).safeTransfer(_msgSender(), refund1);
+            IERC20(token1).safeTransfer(sender, refund1);
+        }
+
+        //refund eth
+        if (txValue > 0) {
+            INonfungiblePositionManager(_underlyingAsset).refundETH();
+            uint256 ethBalance = address(this).balance;
+            if (ethBalance > 0) {
+                _safeTransferETH(sender, ethBalance);
+            }
         }
     }
 
@@ -232,7 +259,7 @@ contract NTokenUniswapV3 is NToken {
         uint256 amountAdd1,
         uint256 amount0Min,
         uint256 amount1Min
-    ) external {
+    ) external payable {
         // interact with Uniswap V3
         _increaseLiquidityCurrentRange(
             tokenId,
@@ -256,13 +283,21 @@ contract NTokenUniswapV3 is NToken {
         uint256 tokenId,
         uint128 liquidityDecrease,
         uint256 amount0Min,
-        uint256 amount1Min
+        uint256 amount1Min,
+        bool receiveEthAsWeth
     ) external {
         // only the token owner of the NToken can decrease the underlying
-        require(_msgSender() == ownerOf(tokenId), Errors.NOT_THE_OWNER);
+        address sender = _msgSender();
+        require(sender == ownerOf(tokenId), Errors.NOT_THE_OWNER);
 
         // interact with Uniswap V3
-        _decreaseLiquidity(tokenId, liquidityDecrease, amount0Min, amount1Min);
+        _decreaseLiquidity(
+            tokenId,
+            liquidityDecrease,
+            amount0Min,
+            amount1Min,
+            receiveEthAsWeth
+        );
 
         // return data about the users healthFactor after decrease
         (
@@ -279,7 +314,7 @@ contract NTokenUniswapV3 is NToken {
             // healthFactor
             uint256 healthFactor, // erc721HealthFactor
 
-        ) = POOL.getUserAccountData(_msgSender());
+        ) = POOL.getUserAccountData(sender);
 
         // revert if decrease would result in a liquidation
         require(
@@ -287,4 +322,22 @@ contract NTokenUniswapV3 is NToken {
             Errors.HEALTH_FACTOR_LOWER_THAN_LIQUIDATION_THRESHOLD
         );
     }
+
+    function checkAllownance(address token) internal {
+        uint256 allownance = IERC20(token).allowance(
+            address(this),
+            _underlyingAsset
+        );
+        if (allownance == 0) {
+            uint256 MAX_INT = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+            IERC20(token).safeApprove(_underlyingAsset, MAX_INT);
+        }
+    }
+
+    function _safeTransferETH(address to, uint256 value) internal {
+        (bool success, ) = to.call{value: value}(new bytes(0));
+        require(success, "ETH_TRANSFER_FAILED");
+    }
+
+    receive() external payable {}
 }
