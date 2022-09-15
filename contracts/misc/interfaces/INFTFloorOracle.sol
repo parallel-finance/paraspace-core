85ea4a834813055cae806b1fb5cb9ac02fdf903d
ronyang <yrong1997@gmail.com>
2022-09-15 13:07:41 +0000

    fix: ci failure

--- contracts/misc/interfaces/INFTFloorOracle.sol
+++ contracts/misc/interfaces/INFTFloorOracle.sol
@@ -2,10 +2,10 @@
 pragma solidity 0.8.10;
 
 interface INFTFloorOracle {
-	function getTwap(address token) external view returns (uint128 price);
+    function getTwap(address token) external view returns (uint128 price);
 
-	function getLastUpdateTime(address token)
-		external
-		view
-		returns (uint128 timestamp);
+    function getLastUpdateTime(address token)
+        external
+        view
+        returns (uint128 timestamp);
 }
