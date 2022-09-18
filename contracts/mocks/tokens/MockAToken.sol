bcbb803f99099029fa49e169bf49fc4cfb454fd3
Walid <walid.wah@gmail.com>
2022-09-17 23:38:00 -0700

    Adding support for rebasing tokens (#13)
    * feat(reabsing tokens): adds a rebasing token logic and AToken and stETH implementations of it
    
    * feat(rebasing tokens): fix typo
    
    * feat(rebasing tokens): fix typo
    
    * feat(support rebasing tokens): adds mock stETH
    
    * feat(support rebasing tokens): move ILido interface
    
    * feat(support rebasing tokens): minor comment
    
    * chore: add MockAToken
    
    * chore: divide mock seth shares by a RAY
    
    * fix: default incomeIndex for MockAToken
    
    * chore: adds rebasing debt token impl
    
    Co-authored-by: zhoujia6139 <zhoujia6139@126.com>

---
+++ contracts/mocks/tokens/MockAToken.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.10;
+
+import {MintableERC20} from "./MintableERC20.sol";
+import {WadRayMath} from "../../protocol/libraries/math/WadRayMath.sol";
+
+contract MockAToken is MintableERC20 {
+
+    uint256 public incomeIndex;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals
+    )  MintableERC20(name, symbol, decimals){
+        incomeIndex = WadRayMath.RAY;
+    }
+
+    function setIncomeIndex(uint256 _incomeIndex) external{
+        incomeIndex = _incomeIndex;
+    }
+
+    function getReserveNormalizedIncome(address asset)
+    external
+    view
+    returns (uint256) {
+        return incomeIndex;
+    }
+
+    function POOL() external view returns (address){
+        return address(this);
+    }
+
+    function UNDERLYING_ASSET_ADDRESS() external view returns (address) {
+        return address(this);
+    }
+}
