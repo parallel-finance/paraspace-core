b4eef0e9fb739e46cc79284197b718c7b43c8d2a
zhoujia6139 <zhoujia6139@126.com>
2022-09-21 09:46:43 +0800

    chore: add nestingOpen check for NTokenMoonBirds

--- contracts/protocol/tokenization/NTokenMoonBirds.sol
+++ contracts/protocol/tokenization/NTokenMoonBirds.sol
@@ -112,4 +112,12 @@ contract NTokenMoonBirds is NToken, IMoonBirdBase {
     {
         return IMoonBird(_underlyingAsset).nestingPeriod(tokenId);
     }
+
+    /**
+        @dev an additional function that is custom to MoonBirds reserve.
+        This function check if nesting is open for the underlying tokens
+    */
+    function nestingOpen() external returns (bool) {
+        return IMoonBird(_underlyingAsset).nestingOpen();
+    }
 }
