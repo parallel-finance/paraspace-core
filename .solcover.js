607d3b37a7eeba6fa06e173128b1850eda3a15fc
Ivan Solomonoff <ivan@parallel.fi>
2022-10-04 15:56:29 -0300

    ci: configured solcover

--- .solcover.js
+++ .solcover.js
@@ -1,4 +1,12 @@
 module.exports = {
-  skipFiles: ["dependencies", "mocks", "misc", "deployments"],
+  mocha: {
+    grep: "@skip-on-coverage",
+    invert: true,
+  },
+  skipFiles: [
+    "contracts/dependencies",
+    "contracts/mocks",
+    "contracts/deployments",
+  ],
   configureYulOptimizer: true,
 };
