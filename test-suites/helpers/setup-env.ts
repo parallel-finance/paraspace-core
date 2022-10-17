e88530a5624ceb5704c2c10ad485723bb71c5b6e
alannotnerd <alan1995wang@outlook.com>
2022-10-17 23:02:36 +0800

    chore: remove hardcode env

--- test-suites/helpers/setup-env.ts
+++ test-suites/helpers/setup-env.ts
@@ -1,9 +1,7 @@
 import hre from "hardhat";
-import {setDRE} from "../../deploy/helpers/misc-utils";
 import {initializeMakeSuite} from "./make-suite";
 
 export async function testEnvFixture() {
-  setDRE(hre);
   await hre.run("deploy:all");
   return await initializeMakeSuite();
 }
