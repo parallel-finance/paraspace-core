e01310c1538ae96e9c59d1a2c67a8957c7249ca0
Cheng JIANG <alex_cj96@foxmail.com>
2022-10-25 21:55:43 +0800

    feat: prepare fallback oracle for mainnet
    Signed-off-by: Cheng JIANG <alex_cj96@foxmail.com>

--- tasks/deployments/03_faucet.ts
+++ tasks/deployments/03_faucet.ts
@@ -1,5 +1,4 @@
 import {task} from "hardhat/config";
-import {isLocalTestnet, isPublicTestnet} from "../../deploy/helpers/contracts-helpers";
 
 const verify = process.env.ETHERSCAN_VERIFICATION === "true" ? true : false;
 
@@ -8,10 +7,8 @@ task(
   "Deploy faucet for mocked ERC20 & ERC721 tokens"
 ).setAction(async (_, DRE) => {
   await DRE.run("set-DRE");
-  if (isLocalTestnet(DRE) || isPublicTestnet(DRE)) {
-    const {step_03} = await import(
-      "../../deploy/tasks/deployments/full-deployment/steps/03_faucet"
-    );
-    await step_03(verify);
-  }
+  const {step_03} = await import(
+    "../../deploy/tasks/deployments/full-deployment/steps/03_faucet"
+  );
+  await step_03(verify);
 });
