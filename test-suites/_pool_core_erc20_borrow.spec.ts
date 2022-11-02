3308010748e6e5ae81f4aaf3b367bd96ab6a0375
Allen <32129782+guoyupeng01@users.noreply.github.com>
2022-11-02 19:31:33 +0800

    Test/optimize fixture and case description (#130)
    * test: refactor erc20 TestCode and Split module
    
    * test: refactor erc20 TestCode and Split module
    
    * test: optimizing declared variables
    
    * test: delete erc20 file
    
    * chore: update descriptions for UTs of erc20 borrowing
    
    * chore: update fixme suggestions
    
    * test: resolve fixme comments
    
    * chore: relove fixme
    
    Co-authored-by: alannotnerd <alan1995wang@outlook.com>

--- test-suites/_pool_core_erc20_borrow.spec.ts
+++ test-suites/_pool_core_erc20_borrow.spec.ts
@@ -1,89 +1,54 @@
 import {loadFixture} from "@nomicfoundation/hardhat-network-helpers";
 import {expect} from "chai";
-import {BigNumber} from "ethers";
+import {parseEther} from "ethers/lib/utils";
 import {MAX_UINT_AMOUNT, ONE_YEAR} from "../deploy/helpers/constants";
 import {convertToCurrencyDecimals} from "../deploy/helpers/contracts-helpers";
 import {advanceTimeAndBlock} from "../deploy/helpers/misc-utils";
 import {ProtocolErrors} from "../deploy/helpers/types";
 import {TestEnv} from "./helpers/make-suite";
 import {testEnvFixture} from "./helpers/setup-env";
-import {
-  borrowAndValidate,
-  supplyAndValidate,
-  repayAndValidate,
-} from "./helpers/validated-steps";
+import {borrowAndValidate, supplyAndValidate} from "./helpers/validated-steps";
+
+const fixture = async () => {
+  const testEnv = await loadFixture(testEnvFixture);
+  const {
+    dai,
+    users: [user1],
+  } = testEnv;
+
+  // User 1 - Deposit dai
+  await supplyAndValidate(dai, "20000", user1, true);
+
+  return testEnv;
+};
 
 describe("pToken/debtToken Borrow Event Accounting", () => {
   const firstDaiDeposit = "10000";
   const secondDaiDeposit = "20000";
-  let accruedInterest = BigNumber.from(0);
-  let testEnv: TestEnv;
 
-  before("Initialize Depositors", async () => {
-    testEnv = await loadFixture(testEnvFixture);
-  });
-
-  it("TC-erc20-borrow-01 User 1 deposits 20k DAI and borrows 8K DAI", async () => {
+  it("TC-erc20-borrow-01 User 1 shouldn't borrow 16k usdc which would exceed current pool liquidity (should fail)", async () => {
     const {
-      dai,
-      users: [user1],
-    } = testEnv;
-
-    // User 1 - Deposit dai
-    await supplyAndValidate(dai, secondDaiDeposit, user1, true);
-
-    // User 1 - Borrow dai
-    await borrowAndValidate(dai, "8000", user1);
-  });
-
-  it("TC-erc20-borrow-02 User 1 acquires share of borrower interest", async () => {
-    const {
-      pDai,
-      users: [user1],
-    } = testEnv;
-    const pDaiBalanceBefore = await pDai.balanceOf(user1.address);
-
-    // Advance time and blocks
-    await advanceTimeAndBlock(parseInt(ONE_YEAR));
-
-    // User 1 - pDAI balance should have increased
-    const pDaiBalanceAfter = await pDai.balanceOf(user1.address);
-    expect(pDaiBalanceAfter).to.be.gt(pDaiBalanceBefore);
-    accruedInterest = pDaiBalanceAfter.sub(pDaiBalanceBefore);
-  });
-
-  it("TC-erc20-borrow-03 User 1 borrow has No liquidity  tokens ", async () => {
-    const {
-      wBTC,
-      users: [user1],
-    } = testEnv;
-
-    // User 1 - Borrow wBTC
-    await expect(borrowAndValidate(wBTC, "0.1", user1)).to.be.reverted;
-  });
-
-  it("TC-erc20-borrow-04 User 1 Borrow 21K is greater than the max liquidity 20K amount  (should fail)", async () => {
-    const {
-      dai,
       pool,
       users: [user1],
-    } = testEnv;
-    const amount = await convertToCurrencyDecimals(dai.address, "20000");
+      usdc,
+    } = await loadFixture(fixture);
+    const amount = await convertToCurrencyDecimals(usdc.address, "100");
+    //FIXME(alan): may we have a error code for this.
     await expect(
       pool
         .connect(user1.signer)
-        .borrow(dai.address, amount, "0", user1.address, {
+        .borrow(usdc.address, amount, "0", user1.address, {
           gasLimit: 5000000,
         })
     ).to.be.reverted;
   });
 
-  it("TC-erc20-borrow-05 User 1 Borrow 21K is greater than the max borrow limit 20K amount  (should fail)", async () => {
+  it("TC-erc20-borrow-02 User 1 shouldn't borrow 20k USDC which would exceed his borrow limit (should fail)", async () => {
     const {
       dai,
       pool,
       users: [user1],
-    } = testEnv;
+    } = await loadFixture(fixture);
     const amount = await convertToCurrencyDecimals(
       dai.address,
       secondDaiDeposit
@@ -97,57 +62,13 @@ describe("pToken/debtToken Borrow Event Accounting", () => {
     ).to.be.revertedWith(ProtocolErrors.COLLATERAL_CANNOT_COVER_NEW_BORROW);
   });
 
-  it("TC-erc20-borrow-06 User 1 partially repays the accrued interest on borrowed DAI", async () => {
-    const {
-      users: [user1],
-      dai,
-    } = testEnv;
-
-    await repayAndValidate(dai, "1000", user1);
-  });
-
-  it("TC-erc20-borrow-07 User 1 still acquires share of borrower interest, but now less", async () => {
-    const {
-      pDai,
-      users: [user1],
-    } = testEnv;
-    const pDaiBalance = await pDai.balanceOf(user1.address);
-
-    // Advance time and blocks
-    await advanceTimeAndBlock(parseInt(ONE_YEAR));
-
-    const pDaiBalanceAfter = await pDai.balanceOf(user1.address);
-    expect(pDaiBalanceAfter).to.be.gt(pDaiBalance);
-
-    // User 1 - interest should be decreased
-    expect(pDaiBalanceAfter.sub(pDaiBalance)).to.be.lt(accruedInterest);
-  });
-
-  it("TC-erc20-borrow-08 User 1 If the debt is not exceeded, Ptoken can transfer", async () => {
-    const {
-      dai,
-      pDai,
-      users: [user1, , user3],
-    } = testEnv;
-    const amount = await convertToCurrencyDecimals(dai.address, "100");
-
-    const pDaiBalance = await pDai.balanceOf(user3.address);
-    // User 1 transfer to User 3
-    await pDai.connect(user1.signer).transfer(user3.address, amount);
-
-    const pDaiBalanceAfter = await pDai.balanceOf(user3.address);
-
-    //  User 3 - pDAI balance should be increased
-    expect(pDaiBalanceAfter).to.be.equal(pDaiBalance.add(amount));
-  });
-
-  it("TC-erc20-borrow-09 User 2 Users do not supply asset to borrow (should fail) ", async () => {
+  it("TC-erc20-borrow-03 User 2 tries to borrow without supply asset (should fail) ", async () => {
     const {
       dai,
       pool,
       users: [, user2],
       variableDebtDai,
-    } = testEnv;
+    } = await loadFixture(fixture);
     const debtBalanceBeforeWithdraw = await variableDebtDai.balanceOf(
       user2.address
     );
@@ -177,12 +98,13 @@ describe("pToken/debtToken Borrow Event Accounting", () => {
     expect(debtBalanceBeforeWithdraw).to.equal(debtBalanceAfterWithdraw);
   });
 
-  it("TC-erc20-borrow-10 User 2 borrow different tokens ", async () => {
+  it("TC-erc20-borrow-04 User 2 tries to borrow different tokens ", async () => {
     const {
       ape,
       usdt,
       users: [user1, user2],
-    } = testEnv;
+    } = await loadFixture(fixture);
+
     // User 2 - deposit liquidity
     await supplyAndValidate(usdt, firstDaiDeposit, user2, true);
     await supplyAndValidate(ape, firstDaiDeposit, user2, true);
@@ -190,5 +112,78 @@ describe("pToken/debtToken Borrow Event Accounting", () => {
     // User 1 - Borrow ape、usdt
     await borrowAndValidate(ape, "100", user1);
     await borrowAndValidate(usdt, "100", user1);
+  });
+
+  describe("borrow erc20 token unit case", () => {
+    const secondDaiDeposit = "20000";
+    let testEnv: TestEnv;
+
+    before("Initialize Depositors", async () => {
+      testEnv = await loadFixture(fixture);
+    });
+
+    it("TC-erc20-borrow-05 User 1 borrows 8K DAI", async () => {
+      const {
+        dai,
+        users: [user1],
+      } = testEnv;
+
+      // User 1 - Borrow dai
+      await borrowAndValidate(dai, "8000", user1);
+    });
+
+    it("TC-erc20-borrow-06 User 1 acquires share of borrower interest", async () => {
+      const {
+        pDai,
+        users: [user1],
+      } = testEnv;
+      const pDaiBalanceBefore = await pDai.balanceOf(user1.address);
+
+      // Advance time and blocks
+      await advanceTimeAndBlock(parseInt(ONE_YEAR));
+
+      // User 1 - pDAI balance should have increased
+      const pDaiBalanceAfter = await pDai.balanceOf(user1.address);
+      expect(pDaiBalanceAfter).to.be.gt(pDaiBalanceBefore);
+    });
+
+    it("TC-erc20-borrow-07 User 1 If the debt is not exceeded, Ptoken can transfer", async () => {
+      const {
+        dai,
+        pDai,
+        users: [user1, , user3],
+      } = testEnv;
+      const amount = await convertToCurrencyDecimals(dai.address, "100");
+
+      const pDaiBalance = await pDai.balanceOf(user3.address);
+      // User 1 transfer to User 3
+      await pDai.connect(user1.signer).transfer(user3.address, amount);
+
+      const pDaiBalanceAfter = await pDai.balanceOf(user3.address);
+
+      //  User 3 - pDAI balance should be increased
+      expect(pDaiBalanceAfter).to.be.equal(pDaiBalance.add(amount));
+    });
+
+    it("TC-erc20-borrow-08 User 2 borrow has reached the liquidation threshold", async () => {
+      const {
+        usdc,
+        pool,
+        users: [, user2],
+      } = testEnv;
+      // User 2 - Deposit usdc
+      await supplyAndValidate(usdc, secondDaiDeposit, user2, true);
+
+      // User 2 - Borrow usdc
+      await borrowAndValidate(usdc, "16000", user2);
+
+      // user1 - healthFactor value is between 1.1 - 1.0
+      const healthFactor = (await pool.getUserAccountData(user2.address))
+        .healthFactor;
+
+      expect(healthFactor)
+        .to.be.most(parseEther("1.1"))
+        .to.be.least(parseEther("1.0"));
+    });
   });
 });
--- test-suites/_pool_core_erc20_supply.spec.ts
+++ test-suites/_pool_core_erc20_supply.spec.ts
@@ -3,7 +3,6 @@ import {expect} from "chai";
 import {parseEther} from "ethers/lib/utils";
 import {MAX_UINT_AMOUNT} from "../deploy/helpers/constants";
 import {convertToCurrencyDecimals} from "../deploy/helpers/contracts-helpers";
-import {TestEnv} from "./helpers/make-suite";
 import {testEnvFixture} from "./helpers/setup-env";
 import {
   mintAndValidate,
@@ -11,42 +10,37 @@ import {
   switchCollateralAndValidate,
 } from "./helpers/validated-steps";
 
-describe("pToken supply Event Accounting", () => {
+describe("pToken Supply Event Accounting", () => {
   const firstDaiDeposit = "10000";
   const secondDaiDeposit = "20000";
-  let testEnv: TestEnv;
-
-  before("Initialize Depositors", async () => {
-    testEnv = await loadFixture(testEnvFixture);
-  });
 
   it("TC-erc20-supply-01 User 1 deposits 10k DAI", async () => {
     const {
       dai,
       users: [user1],
-    } = testEnv;
+    } = await loadFixture(testEnvFixture);
 
     // User 2 - Deposit dai
     await supplyAndValidate(dai, firstDaiDeposit, user1, true);
   });
 
-  it("TC-erc20-supply-02 User 1 multiple deposits 20k DAI", async () => {
+  it("TC-erc20-supply-02 User1 supplies in batches", async () => {
     const {
       dai,
       users: [user1],
-    } = testEnv;
+    } = await loadFixture(testEnvFixture);
 
-    // User 2 - Deposit dai
+    // User 2 - multiple Deposit dai
     await supplyAndValidate(dai, secondDaiDeposit, user1, true);
     await supplyAndValidate(dai, secondDaiDeposit, user1, true);
   });
 
-  it("TC-erc20-supply-03 User 1 supply 10k exceed than approve 5k (should fail)", async () => {
+  it("TC-erc20-supply-03 User1 shouldn't supply dai more than its allowance (should fail)", async () => {
     const {
       dai,
       pool,
       users: [user1],
-    } = testEnv;
+    } = await loadFixture(testEnvFixture);
     const amount = await convertToCurrencyDecimals(
       dai.address,
       firstDaiDeposit
@@ -62,17 +56,21 @@ describe("pToken supply Event Accounting", () => {
     const {
       users: [user1],
       dai,
-    } = testEnv;
+    } = await loadFixture(testEnvFixture);
 
+    // User 1 -  Deposit dai
+    await supplyAndValidate(dai, "200", user1, true);
+
+    // User 1 -  close Collateral
     await switchCollateralAndValidate(user1, dai, false);
   });
 
-  it("TC-erc20-supply-05 User 2 not Obtain approve to Operate supplying  10K (should fail)", async () => {
+  it("TC-erc20-supply-05 User 2 tries to supply without obtaining approval (should fail)", async () => {
     const {
       dai,
       pool,
       users: [, user2],
-    } = testEnv;
+    } = await loadFixture(testEnvFixture);
     await mintAndValidate(dai, firstDaiDeposit, user2);
     await expect(
       pool
@@ -81,12 +79,12 @@ describe("pToken supply Event Accounting", () => {
     ).to.be.revertedWith("ERC20: transfer amount exceeds allowance");
   });
 
-  it("TC-erc20-supply-06 User 1 supply 20K exceeds than user balance   (should fail) ", async () => {
+  it("TC-erc20-supply-06 User 1 tries to supply 20K exceed the balance amount (should fail)", async () => {
     const {
       dai,
       pool,
       users: [user1],
-    } = testEnv;
+    } = await loadFixture(testEnvFixture);
     const amount = await convertToCurrencyDecimals(dai.address, "200000");
 
     await dai.connect(user1.signer).approve(pool.address, MAX_UINT_AMOUNT);
@@ -95,12 +93,12 @@ describe("pToken supply Event Accounting", () => {
     ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
   });
 
-  it("TC-erc20-supply-07 User 1 Supply not minted token (should fail) ", async () => {
+  it("TC-erc20-supply-07 User 1 Supply not minted token (should fail)", async () => {
     const {
       usdt,
       pool,
       users: [user1],
-    } = testEnv;
+    } = await loadFixture(testEnvFixture);
     const amount = await convertToCurrencyDecimals(usdt.address, "200000");
 
     await usdt.connect(user1.signer).approve(pool.address, MAX_UINT_AMOUNT);
@@ -111,12 +109,13 @@ describe("pToken supply Event Accounting", () => {
     ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
   });
 
-  it("TC-erc20-supply-08 User 1 tries to send the pToken to User 2 (no borrow)", async () => {
+  it("TC-erc20-supply-08 User 1 tries tansfer the pToken to User 2 (no borrow)", async () => {
     const {
       pDai,
       dai,
       users: [user1, user2],
-    } = testEnv;
+    } = await loadFixture(testEnvFixture);
+    await supplyAndValidate(dai, "200", user1, true);
     const user1balance = await pDai.balanceOf(user1.address);
     const user2balance = await pDai.balanceOf(user2.address);
     const amount = await convertToCurrencyDecimals(pDai.address, "100");
