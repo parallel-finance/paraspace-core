9694a218ac0fa82a32da87bcc75343614a28ffdd
Alan WANG <alan1995wang@outlook.com>
2022-11-03 21:04:22 +0800

    feat: refactor ptoken admin modifier tests

--- test-suites/_xtoken_ptoken.spec.ts
+++ test-suites/_xtoken_ptoken.spec.ts
@@ -8,30 +8,30 @@ import {
 import {ProtocolErrors} from "../deploy/helpers/types";
 import {testEnvFixture} from "./helpers/setup-env";
 
-const fixture = async () => {
-  const testEnv = await loadFixture(testEnvFixture);
-  const {pool} = testEnv;
-  const delegationERC20 = await deployMintableDelegationERC20([
-    "DEL",
-    "DEL",
-    "18",
-  ]);
-  const delegationPToken = await deployDelegationAwarePToken([
-    pool.address,
-    delegationERC20.address,
-    ZERO_ADDRESS,
-    ZERO_ADDRESS,
-    "aDEL",
-    "aDEL",
-  ]);
-  return {
-    ...testEnv,
-    delegationERC20,
-    delegationPToken,
+describe("Ptoken delegation", () => {
+  const fixture = async () => {
+    const testEnv = await loadFixture(testEnvFixture);
+    const {pool} = testEnv;
+    const delegationERC20 = await deployMintableDelegationERC20([
+      "DEL",
+      "DEL",
+      "18",
+    ]);
+    const delegationPToken = await deployDelegationAwarePToken([
+      pool.address,
+      delegationERC20.address,
+      ZERO_ADDRESS,
+      ZERO_ADDRESS,
+      "aDEL",
+      "aDEL",
+    ]);
+    return {
+      ...testEnv,
+      delegationERC20,
+      delegationPToken,
+    };
   };
-};
 
-describe("Ptoken behaviors", () => {
   it("TC-ptoken-delegation-aware-01: user shouldn't call delegateUnderlyingTo if he isn't POOL_ADMIN", async () => {
     const {
       users: [user1, user2],
@@ -56,5 +56,35 @@ describe("Ptoken behaviors", () => {
     const delegateeAddress = await delegationERC20.delegatee();
 
     expect(delegateeAddress).to.be.equal(user2.address);
+  });
+});
+
+describe("Ptoken modifiers", () => {
+  it("TC-ptoken-access-control-01: mint should only be called by POOL", async () => {
+    const {deployer, pDai} = await loadFixture(testEnvFixture);
+    await expect(
+      pDai.mint(deployer.address, deployer.address, "1", "1")
+    ).to.be.revertedWith(ProtocolErrors.CALLER_MUST_BE_POOL);
+  });
+
+  it("TC-ptoken-access-control-02: burn should only be called by POOL", async () => {
+    const {deployer, pDai} = await loadFixture(testEnvFixture);
+    await expect(
+      pDai.burn(deployer.address, deployer.address, "1", "1")
+    ).to.be.revertedWith(ProtocolErrors.CALLER_MUST_BE_POOL);
+  });
+
+  it("TC-ptoken-access-control-03: transferOnLiquidation should only be called by POOL", async () => {
+    const {deployer, users, pDai} = await loadFixture(testEnvFixture);
+    await expect(
+      pDai.transferOnLiquidation(deployer.address, users[0].address, "1")
+    ).to.be.revertedWith(ProtocolErrors.CALLER_MUST_BE_POOL);
+  });
+
+  it("TC-ptoken-access-control-04: transferUnderlyingTo should only be called by POOL", async () => {
+    const {deployer, pDai} = await loadFixture(testEnvFixture);
+    await expect(
+      pDai.transferUnderlyingTo(deployer.address, "1")
+    ).to.be.revertedWith(ProtocolErrors.CALLER_MUST_BE_POOL);
   });
 });
