7c2955a152c243ff0fd690f6fc11558fd2574775
Alan WANG <alan1995wang@outlook.com>
2022-11-04 14:53:40 +0800

    feat: refactor ptoken permit tests

--- test-suites/_xtoken_ptoken.spec.ts
+++ test-suites/_xtoken_ptoken.spec.ts
@@ -1,13 +1,24 @@
+import type {HardhatRuntimeEnvironment} from "hardhat/types";
+
 import {loadFixture} from "@nomicfoundation/hardhat-network-helpers";
 import {expect} from "chai";
-import {ZERO_ADDRESS} from "../deploy/helpers/constants";
+import {utils} from "ethers";
+import {MAX_UINT_AMOUNT, ZERO_ADDRESS} from "../deploy/helpers/constants";
 import {
   deployDelegationAwarePToken,
   deployMintableDelegationERC20,
 } from "../deploy/helpers/contracts-deployments";
+import {waitForTx} from "../deploy/helpers/misc-utils";
 import {ProtocolErrors} from "../deploy/helpers/types";
 import {testEnvFixture} from "./helpers/setup-env";
+import {getTestWallets} from "./helpers/utils/wallets";
+import {HARDHAT_CHAINID} from "../deploy/helpers/hardhat-constants";
+import {
+  buildPermitParams,
+  getSignatureFromTypedData,
+} from "../deploy/helpers/contracts-helpers";
 
+declare let hre: HardhatRuntimeEnvironment;
 describe("Ptoken delegation", () => {
   const fixture = async () => {
     const testEnv = await loadFixture(testEnvFixture);
@@ -86,5 +97,345 @@ describe("Ptoken modifiers", () => {
     await expect(
       pDai.transferUnderlyingTo(deployer.address, "1")
     ).to.be.revertedWith(ProtocolErrors.CALLER_MUST_BE_POOL);
+  });
+});
+
+describe("Functionalities of ptoken permit", () => {
+  const fixture = async () => {
+    const testEnv = await loadFixture(testEnvFixture);
+    const {dai, pool, deployer} = testEnv;
+    const testWallets = getTestWallets();
+    // Mint DAI and deposit to Pool to for pDAI
+    await waitForTx(await dai["mint(uint256)"](utils.parseEther("20000")));
+    await waitForTx(await dai.approve(pool.address, utils.parseEther("20000")));
+    await waitForTx(
+      await pool.supply(
+        dai.address,
+        utils.parseEther("20000"),
+        deployer.address,
+        0
+      )
+    );
+    return {
+      ...testEnv,
+      ownerPrivateKey: testWallets[0].secretKey,
+    };
+  };
+  const EIP712_REVISION = "1";
+
+  it("TC-ptoken-permit-01: user shouldn't submit a permit with 0 expiration", async () => {
+    const {
+      pDai,
+      deployer: owner,
+      users: [, spender],
+      ownerPrivateKey,
+    } = await loadFixture(fixture);
+
+    const tokenName = await pDai.name();
+
+    const chainId = hre.network.config.chainId || HARDHAT_CHAINID;
+    const expiration = 0;
+    const nonce = (await pDai.nonces(owner.address)).toNumber();
+    const permitAmount = utils.parseEther("2").toString();
+    const msgParams = buildPermitParams(
+      chainId,
+      pDai.address,
+      EIP712_REVISION,
+      tokenName,
+      owner.address,
+      spender.address,
+      nonce,
+      permitAmount,
+      expiration.toFixed()
+    );
+
+    expect(
+      (await pDai.allowance(owner.address, spender.address)).toString()
+    ).to.be.equal("0", "INVALID_ALLOWANCE_BEFORE_PERMIT");
+
+    const {v, r, s} = getSignatureFromTypedData(ownerPrivateKey, msgParams);
+
+    await expect(
+      pDai
+        .connect(spender.signer)
+        .permit(
+          owner.address,
+          spender.address,
+          permitAmount,
+          expiration,
+          v,
+          r,
+          s
+        )
+    ).to.be.revertedWith(ProtocolErrors.INVALID_EXPIRATION);
+
+    expect(
+      (await pDai.allowance(owner.address, spender.address)).toString()
+    ).to.be.equal("0", "INVALID_ALLOWANCE_AFTER_PERMIT");
+  });
+
+  describe("Allowance could be override", () => {
+    let preset: Awaited<ReturnType<typeof fixture>>;
+    before(async () => {
+      preset = await loadFixture(fixture);
+    });
+
+    it("TC-ptoken-permit-02: user should submit a permit with maxmium expiration", async () => {
+      const {
+        pDai,
+        deployer: owner,
+        users: [, spender],
+        ownerPrivateKey,
+      } = preset;
+
+      const chainId = hre.network.config.chainId || HARDHAT_CHAINID;
+      const deadline = MAX_UINT_AMOUNT;
+      const nonce = (await pDai.nonces(owner.address)).toNumber();
+      const permitAmount = utils.parseEther("2").toString();
+      const msgParams = buildPermitParams(
+        chainId,
+        pDai.address,
+        EIP712_REVISION,
+        await pDai.name(),
+        owner.address,
+        spender.address,
+        nonce,
+        deadline,
+        permitAmount
+      );
+
+      expect(
+        (await pDai.allowance(owner.address, spender.address)).toString()
+      ).to.be.equal("0", "INVALID_ALLOWANCE_BEFORE_PERMIT");
+
+      const {v, r, s} = getSignatureFromTypedData(ownerPrivateKey, msgParams);
+
+      expect(
+        await pDai
+          .connect(spender.signer)
+          .permit(
+            owner.address,
+            spender.address,
+            permitAmount,
+            deadline,
+            v,
+            r,
+            s
+          )
+      );
+
+      expect((await pDai.nonces(owner.address)).toNumber()).to.be.equal(1);
+    });
+
+    it("TC-ptoken-permit-03: user should override a permit with new params", async () => {
+      const {
+        pDai,
+        deployer: owner,
+        users: [, spender],
+        ownerPrivateKey,
+      } = preset;
+
+      const chainId = hre.network.config.chainId || HARDHAT_CHAINID;
+      const deadline = MAX_UINT_AMOUNT;
+      const nonce = (await pDai.nonces(owner.address)).toNumber();
+      const permitAmount = "0";
+      const msgParams = buildPermitParams(
+        chainId,
+        pDai.address,
+        EIP712_REVISION,
+        await pDai.name(),
+        owner.address,
+        spender.address,
+        nonce,
+        deadline,
+        permitAmount
+      );
+
+      expect(
+        (await pDai.allowance(owner.address, spender.address)).toString()
+      ).to.be.equal(utils.parseEther("2"), "INVALID_ALLOWANCE_BEFORE_PERMIT");
+
+      const {v, r, s} = getSignatureFromTypedData(ownerPrivateKey, msgParams);
+      expect(
+        await pDai
+          .connect(spender.signer)
+          .permit(
+            owner.address,
+            spender.address,
+            permitAmount,
+            deadline,
+            v,
+            r,
+            s
+          )
+      );
+      expect(
+        (await pDai.allowance(owner.address, spender.address)).toString()
+      ).to.be.equal(permitAmount, "INVALID_ALLOWANCE_AFTER_PERMIT");
+
+      expect((await pDai.nonces(owner.address)).toNumber()).to.be.equal(2);
+    });
+  });
+
+  it("TC-ptoken-permit-04: user shouldn't submit a permit with invalid nonce", async () => {
+    const {
+      pDai,
+      deployer: owner,
+      users: [, spender],
+      ownerPrivateKey,
+    } = await loadFixture(fixture);
+
+    const chainId = hre.network.config.chainId || HARDHAT_CHAINID;
+    const deadline = MAX_UINT_AMOUNT;
+    const nonce = 1000;
+    const permitAmount = "0";
+    const msgParams = buildPermitParams(
+      chainId,
+      pDai.address,
+      EIP712_REVISION,
+      await pDai.name(),
+      owner.address,
+      spender.address,
+      nonce,
+      deadline,
+      permitAmount
+    );
+
+    const {v, r, s} = getSignatureFromTypedData(ownerPrivateKey, msgParams);
+
+    await expect(
+      pDai
+        .connect(spender.signer)
+        .permit(owner.address, spender.address, permitAmount, deadline, v, r, s)
+    ).to.be.revertedWith(ProtocolErrors.INVALID_SIGNATURE);
+  });
+
+  it("TC-ptoken-permit-05: user shouldn't submit a permit if expiration block is lower than current height", async () => {
+    const {
+      pDai,
+      deployer: owner,
+      users: [, spender],
+      ownerPrivateKey,
+    } = await loadFixture(fixture);
+
+    const chainId = hre.network.config.chainId || HARDHAT_CHAINID;
+    const expiration = "1";
+    const nonce = (await pDai.nonces(owner.address)).toNumber();
+    const permitAmount = "0";
+    const msgParams = buildPermitParams(
+      chainId,
+      pDai.address,
+      EIP712_REVISION,
+      await pDai.name(),
+      owner.address,
+      spender.address,
+      nonce,
+      expiration,
+      permitAmount
+    );
+
+    const {v, r, s} = getSignatureFromTypedData(ownerPrivateKey, msgParams);
+
+    await expect(
+      pDai
+        .connect(spender.signer)
+        .permit(
+          owner.address,
+          spender.address,
+          expiration,
+          permitAmount,
+          v,
+          r,
+          s
+        )
+    ).to.be.revertedWith(ProtocolErrors.INVALID_EXPIRATION);
+  });
+
+  it("TC-ptoken-permit-06: user shouldn't submit a permit if signature doesn't match the params", async () => {
+    const {
+      pDai,
+      deployer: owner,
+      users: [, spender],
+      ownerPrivateKey,
+    } = await loadFixture(fixture);
+
+    const chainId = hre.network.config.chainId || HARDHAT_CHAINID;
+    const deadline = MAX_UINT_AMOUNT;
+    const nonce = (await pDai.nonces(owner.address)).toNumber();
+    const permitAmount = "0";
+    const msgParams = buildPermitParams(
+      chainId,
+      pDai.address,
+      EIP712_REVISION,
+      await pDai.name(),
+      owner.address,
+      spender.address,
+      nonce,
+      deadline,
+      permitAmount
+    );
+
+    const {v, r, s} = getSignatureFromTypedData(ownerPrivateKey, msgParams);
+
+    await expect(
+      pDai
+        .connect(spender.signer)
+        .permit(owner.address, ZERO_ADDRESS, permitAmount, deadline, v, r, s)
+    ).to.be.revertedWith(ProtocolErrors.INVALID_SIGNATURE);
+  });
+
+  it("TC-ptoken-permit-07: user shouldn't submit a permit while owner is ZERO_ADDRESS", async () => {
+    const {
+      pDai,
+      deployer: owner,
+      users: [, spender],
+      ownerPrivateKey,
+    } = await loadFixture(fixture);
+
+    const chainId = hre.network.config.chainId || HARDHAT_CHAINID;
+    const expiration = MAX_UINT_AMOUNT;
+    const nonce = (await pDai.nonces(owner.address)).toNumber();
+    const permitAmount = "0";
+    const msgParams = buildPermitParams(
+      chainId,
+      pDai.address,
+      EIP712_REVISION,
+      await pDai.name(),
+      owner.address,
+      spender.address,
+      nonce,
+      expiration,
+      permitAmount
+    );
+
+    const {v, r, s} = getSignatureFromTypedData(ownerPrivateKey, msgParams);
+
+    await expect(
+      pDai
+        .connect(spender.signer)
+        .permit(
+          ZERO_ADDRESS,
+          spender.address,
+          expiration,
+          permitAmount,
+          v,
+          r,
+          s
+        )
+    ).to.be.revertedWith(ProtocolErrors.ZERO_ADDRESS_NOT_VALID);
+  });
+
+  it("TC-ptoken-permit-08: domain separator should be correct", async () => {
+    const {pDai} = await loadFixture(fixture);
+    const separator = await pDai.DOMAIN_SEPARATOR();
+
+    const domain = {
+      name: await pDai.name(),
+      version: EIP712_REVISION,
+      chainId: hre.network.config.chainId,
+      verifyingContract: pDai.address,
+    };
+    const domainSeparator = utils._TypedDataEncoder.hashDomain(domain);
+    expect(separator).to.be.equal(domainSeparator, "Invalid domain separator");
   });
 });
