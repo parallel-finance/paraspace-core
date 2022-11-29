757d80ae7b420b66a471ff626d15811e3e3ac996
Alan WANG <alan1995wang@outlook.com>
2022-11-29 16:54:38 +0800

    feat: add invariant

--- test/pnm/Supply.t.sol
+++ test/pnm/Supply.t.sol
@@ -6,10 +6,13 @@ import "../../contracts/mocks/tokens/MintableERC20.sol";
 contract SupplyTest is ParaTest {
     function setUp() public {
         vm.createSelectFork("http://localhost:8545");
-    }
-    function testSupply() public {
         MintableERC20 dai = MintableERC20(get("DAI"));
         address user1 = users[0];
         dai.mint(user1, 1000000000);
+    }
+    function invariantSupply() public {
+        MintableERC20 dai = MintableERC20(get("DAI"));
+        address user1 = users[0];
+        require(dai.balanceOf(user1) >= 1000000000);
     }
 }
