5710edd52d0b2e9c6ebedbace8e15c872f7ce8e1
0xwalid <walid.wah@gmail.com>
2022-12-18 16:51:37 -0800

    feat(para-proxy): lint

--- contracts/protocol/libraries/paraspace-upgradeability/ParaProxyInterfaces.sol
+++ contracts/protocol/libraries/paraspace-upgradeability/ParaProxyInterfaces.sol
@@ -17,28 +17,44 @@ contract ParaProxyInterfaces is IParaProxyInterfaces, IERC165 {
 
     /// @notice Gets all facets and their selectors.
     /// @return facets_ Implementation
-    function facets() external override view returns (Implementation[] memory facets_) {
+    function facets()
+        external
+        view
+        override
+        returns (Implementation[] memory facets_)
+    {
         ParaProxyLib.ProxyStorage storage ds = ParaProxyLib.diamondStorage();
         uint256 numFacets = ds.implementationAddresses.length;
         facets_ = new Implementation[](numFacets);
         for (uint256 i; i < numFacets; i++) {
             address facetAddress_ = ds.implementationAddresses[i];
             facets_[i].implAddress = facetAddress_;
-            facets_[i].functionSelectors = ds.implementationFunctionSelectors[facetAddress_].functionSelectors;
+            facets_[i].functionSelectors = ds
+                .implementationFunctionSelectors[facetAddress_]
+                .functionSelectors;
         }
     }
 
     /// @notice Gets all the function selectors provided by a facet.
     /// @param _facet The facet address.
     /// @return facetFunctionSelectors_
-    function facetFunctionSelectors(address _facet) external override view returns (bytes4[] memory facetFunctionSelectors_) {
+    function facetFunctionSelectors(
+        address _facet
+    ) external view override returns (bytes4[] memory facetFunctionSelectors_) {
         ParaProxyLib.ProxyStorage storage ds = ParaProxyLib.diamondStorage();
-        facetFunctionSelectors_ = ds.implementationFunctionSelectors[_facet].functionSelectors;
+        facetFunctionSelectors_ = ds
+            .implementationFunctionSelectors[_facet]
+            .functionSelectors;
     }
 
     /// @notice Get all the facet addresses used by a diamond.
     /// @return facetAddresses_
-    function facetAddresses() external override view returns (address[] memory facetAddresses_) {
+    function facetAddresses()
+        external
+        view
+        override
+        returns (address[] memory facetAddresses_)
+    {
         ParaProxyLib.ProxyStorage storage ds = ParaProxyLib.diamondStorage();
         facetAddresses_ = ds.implementationAddresses;
     }
@@ -47,13 +63,19 @@ contract ParaProxyInterfaces is IParaProxyInterfaces, IERC165 {
     /// @dev If facet is not found return address(0).
     /// @param _functionSelector The function selector.
     /// @return facetAddress_ The facet address.
-    function facetAddress(bytes4 _functionSelector) external override view returns (address facetAddress_) {
+    function facetAddress(
+        bytes4 _functionSelector
+    ) external view override returns (address facetAddress_) {
         ParaProxyLib.ProxyStorage storage ds = ParaProxyLib.diamondStorage();
-        facetAddress_ = ds.selectorToImplAndPosition[_functionSelector].implAddress;
+        facetAddress_ = ds
+            .selectorToImplAndPosition[_functionSelector]
+            .implAddress;
     }
 
     // This implements ERC-165.
-    function supportsInterface(bytes4 _interfaceId) external override view returns (bool) {
+    function supportsInterface(
+        bytes4 _interfaceId
+    ) external view override returns (bool) {
         ParaProxyLib.ProxyStorage storage ds = ParaProxyLib.diamondStorage();
         return ds.supportedInterfaces[_interfaceId];
     }
