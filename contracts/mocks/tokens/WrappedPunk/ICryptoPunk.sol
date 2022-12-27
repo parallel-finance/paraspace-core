077faa6906447255576f1486dccd291eb3307a8d
GopherJ <alex_cj96@foxmail.com>
2022-12-27 20:37:57 +0800

    chore: remove punks adapter related
    Signed-off-by: GopherJ <alex_cj96@foxmail.com>

--- contracts/mocks/tokens/WrappedPunk/ICryptoPunk.sol
+++ contracts/mocks/tokens/WrappedPunk/ICryptoPunk.sol
@@ -2,21 +2,17 @@
 pragma solidity 0.8.10;
 
 interface ICryptoPunk {
-
-    struct Offer {
-        bool isForSale;
-        uint256 punkIndex;
-        address seller;
-        uint256 minValue;
-        address onlySellTo;
-    }
-
-    function punkIndexToAddress(uint256 punkIndex) external view returns (address);
+    function punkIndexToAddress(uint256 punkIndex) external returns (address);
 
     function punksOfferedForSale(uint256 punkIndex)
         external
-        view
-        returns (Offer memory);
+        returns (
+            bool,
+            uint256,
+            address,
+            uint256,
+            address
+        );
 
     function buyPunk(uint256 punkIndex) external payable;
 
