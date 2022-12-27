ceafdc62e96b0d793d98a61e90a72646e3a3b4c3
GopherJ <alex_cj96@foxmail.com>
2022-12-27 16:52:16 +0800

    chore: add sudoswap dep
    Signed-off-by: GopherJ <alex_cj96@foxmail.com>

---
+++ contracts/dependencies/sol-mate/SafeTransferLib.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.10;
+
+library SafeTransferLib {
+    /*//////////////////////////////////////////////////////////////
+                             ETH OPERATIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function safeTransferETH(address to, uint256 amount) internal {
+        bool success;
+
+        /// @solidity memory-safe-assembly
+        assembly {
+            // Transfer the ETH and store if it succeeded or not.
+            success := call(gas(), to, amount, 0, 0, 0, 0)
+        }
+
+        require(success, "ETH_TRANSFER_FAILED");
+    }
+}
