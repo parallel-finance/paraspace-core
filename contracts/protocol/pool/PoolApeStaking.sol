971bd8c04ac565a29f908c4b9029b5fb2a90dee1
zhoujia6139 <zhoujia6139@126.com>
2023-01-04 11:16:48 +0800

    chore: remove unnecessary variable

--- contracts/protocol/pool/PoolApeStaking.sol
+++ contracts/protocol/pool/PoolApeStaking.sol
@@ -501,7 +501,6 @@ contract PoolApeStaking is
         uint256[] amounts;
         address[] originArray;
         uint256 totalAmount;
-        address tmpPositionOwner;
     }
 
     /// @inheritdoc IPoolApeStaking
@@ -527,10 +526,11 @@ contract PoolApeStaking is
             ApeCoinStaking.PairNft[] calldata nftPair = _nftPairs[i];
             localVar.originArray = new address[](nftPair.length);
             for (uint256 j = 0; j < nftPair.length; j++) {
-                localVar.tmpPositionOwner = INToken(localVar.xTokenAddress)
-                    .ownerOf(nftPair[j].mainTokenId);
                 require(
-                    users[i] == localVar.tmpPositionOwner,
+                    users[i] ==
+                        INToken(localVar.xTokenAddress).ownerOf(
+                            nftPair[j].mainTokenId
+                        ),
                     "user is not owner"
                 );
 
