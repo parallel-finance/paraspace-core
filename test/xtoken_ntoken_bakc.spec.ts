ae78f9fbdf6b22684ed9b66f01fcec387f830490
zhoujia6139 <zhoujia6139@126.com>
2023-01-05 23:01:47 +0800

    chore: fix format

--- test/xtoken_ntoken_bakc.spec.ts
+++ test/xtoken_ntoken_bakc.spec.ts
@@ -66,11 +66,11 @@ describe("APE Coin Staking Test", () => {
     // user4 deposit MINIMUM_LIQUIDITY to make test case easy
     await mintAndValidate(ape, "1", user4);
     await waitForTx(
-        await ape.connect(user4.signer).approve(cApe.address, MAX_UINT_AMOUNT)
+      await ape.connect(user4.signer).approve(cApe.address, MAX_UINT_AMOUNT)
     );
     const MINIMUM_LIQUIDITY = await cApe.MINIMUM_LIQUIDITY();
     await waitForTx(
-        await cApe.connect(user4.signer).deposit(user4.address, MINIMUM_LIQUIDITY)
+      await cApe.connect(user4.signer).deposit(user4.address, MINIMUM_LIQUIDITY)
     );
 
     return testEnv;
