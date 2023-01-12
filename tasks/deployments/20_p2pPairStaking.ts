b349a58da1b86905ae7565e25a1da6f62ace513a
zhoujia6139 <zhoujia6139@126.com>
2023-01-12 15:20:07 +0800

    chore: fix format

--- tasks/deployments/20_p2pPairStaking.ts
+++ tasks/deployments/20_p2pPairStaking.ts
@@ -2,11 +2,11 @@ import {task} from "hardhat/config";
 import {ETHERSCAN_VERIFICATION} from "../../helpers/hardhat-constants";
 
 task("deploy:P2PPairStaking", "Deploy P2PPairStaking").setAction(
-    async (_, DRE) => {
-        await DRE.run("set-DRE");
-        const {step_20} = await import(
-            "../../scripts/deployments/steps/20_p2pPairStaking"
-            );
-        await step_20(ETHERSCAN_VERIFICATION);
-    }
+  async (_, DRE) => {
+    await DRE.run("set-DRE");
+    const {step_20} = await import(
+      "../../scripts/deployments/steps/20_p2pPairStaking"
+    );
+    await step_20(ETHERSCAN_VERIFICATION);
+  }
 );
