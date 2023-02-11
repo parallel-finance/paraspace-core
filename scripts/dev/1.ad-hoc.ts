6a3e39cbfafa2889f2bcb82a3629170d98cb9d45
GopherJ <alex_cj96@foxmail.com>
2023-02-11 09:39:52 +0800

    chore: add p2p
    Signed-off-by: GopherJ <alex_cj96@foxmail.com>

--- scripts/dev/1.ad-hoc.ts
+++ scripts/dev/1.ad-hoc.ts
@@ -1,7 +1,12 @@
 import rawBRE from "hardhat";
+import {deployParaSpaceAidrop} from "../../helpers/contracts-deployments";
 
 const adHoc = async () => {
   console.time("ad-hoc");
+  await deployParaSpaceAidrop(
+    "0x4d224452801ACEd8B2F0aebE155379bb5D594381",
+    "1991698504"
+  );
   console.timeEnd("ad-hoc");
 };
 
