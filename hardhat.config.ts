9ad8b2f366567ff36e28531e81f2be2f03860f39
GopherJ <alex_cj96@foxmail.com>
2023-02-16 17:08:20 +0800

    chore: increase runs
    Signed-off-by: GopherJ <alex_cj96@foxmail.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -75,7 +75,7 @@ const hardhatConfig: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200,
+            runs: 2000,
           },
           evmVersion: "london",
         },
