3e01d18739e7513851e9a772b541df344ad9dddd
GopherJ <alex_cj96@foxmail.com>
2023-02-17 20:06:55 +0800

    chore: simulate twap attack
    Signed-off-by: GopherJ <alex_cj96@foxmail.com>

--- scripts/dev/10.send-eth.ts
+++ scripts/dev/10.send-eth.ts
@@ -1,13 +1,11 @@
 import {utils} from "ethers";
 import rawBRE from "hardhat";
 import {getFirstSigner} from "../../helpers/contracts-getters";
-import {getParaSpaceAdmins} from "../../helpers/contracts-helpers";
 import {waitForTx} from "../../helpers/misc-utils";
 
 const sendETH = async () => {
   console.time("send-eth");
   const signer = await getFirstSigner();
-  const {paraSpaceAdminAddress} = await getParaSpaceAdmins();
   await waitForTx(
     await signer.sendTransaction({
       to: "0x6cC5F688a315f3dC28A7781717a9A798a59fDA7b",
