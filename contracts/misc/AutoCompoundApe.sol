256a418a01f596ffb701af949068a7f1dcdaab90
zhoujia6139 <zhoujia6139@126.com>
2023-03-19 09:56:59 +0800

    fix: cApe exchange rate manipulation

--- contracts/misc/AutoCompoundApe.sol
+++ contracts/misc/AutoCompoundApe.sol
@@ -30,6 +30,7 @@ contract AutoCompoundApe is
     ApeCoinStaking public immutable apeStaking;
     IERC20 public immutable apeCoin;
     uint256 public bufferBalance;
+    uint256 public stakingBalance;
 
     constructor(address _apeCoin, address _apeStaking) {
         apeStaking = ApeCoinStaking(_apeStaking);
@@ -94,13 +95,12 @@ contract AutoCompoundApe is
         override
         returns (uint256)
     {
-        (uint256 stakedAmount, ) = apeStaking.addressPosition(address(this));
         uint256 rewardAmount = apeStaking.pendingRewards(
             APE_COIN_POOL_ID,
             address(this),
             0
         );
-        return stakedAmount + rewardAmount + bufferBalance;
+        return stakingBalance + rewardAmount + bufferBalance;
     }
 
     function _withdrawFromApeCoinStaking(uint256 amount) internal {
@@ -108,6 +108,7 @@ contract AutoCompoundApe is
         apeStaking.withdrawSelfApeCoin(amount);
         uint256 balanceAfter = apeCoin.balanceOf(address(this));
         uint256 realWithdraw = balanceAfter - balanceBefore;
+        stakingBalance -= amount;
         bufferBalance += realWithdraw;
     }
 
@@ -125,6 +126,7 @@ contract AutoCompoundApe is
         uint256 _bufferBalance = bufferBalance;
         if (_bufferBalance >= MIN_OPERATION_AMOUNT) {
             apeStaking.depositSelfApeCoin(_bufferBalance);
+            stakingBalance += _bufferBalance;
             bufferBalance = 0;
         }
     }
@@ -177,5 +179,13 @@ contract AutoCompoundApe is
         returns (address)
     {
         return IDelegation(delegateContract).delegation(address(this), spaceId);
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
     }
 }
