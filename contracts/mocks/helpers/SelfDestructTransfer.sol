660acf234108235906650dcf9d275253368f71d5
GopherJ <alex_cj96@foxmail.com>
2023-06-06 23:28:22 +0800

    feat: add back deleted contracts
    Signed-off-by: GopherJ <alex_cj96@foxmail.com>

---
+++ contracts/mocks/helpers/SelfDestructTransfer.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import {Helpers} from "../../protocol/libraries/helpers/Helpers.sol";
+
+contract SelfdestructTransfer {
+    function destroyAndTransfer(address payable to) external payable {
+      Helpers.safeTransferETH(to, address(this).balance);
+    }
+}
