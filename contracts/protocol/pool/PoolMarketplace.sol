a4da24fceaa21f9d6ada99f21b8e852b496f3485
GopherJ <alex_cj96@foxmail.com>
2023-06-09 10:55:23 +0800

    chore: cleanup
    Signed-off-by: GopherJ <alex_cj96@foxmail.com>

--- contracts/protocol/pool/PoolMarketplace.sol
+++ contracts/protocol/pool/PoolMarketplace.sol
@@ -2,33 +2,16 @@
 pragma solidity 0.8.10;
 
 import {ParaVersionedInitializable} from "../libraries/paraspace-upgradeability/ParaVersionedInitializable.sol";
-import {Errors} from "../libraries/helpers/Errors.sol";
-import {ReserveConfiguration} from "../libraries/configuration/ReserveConfiguration.sol";
-import {PoolLogic} from "../libraries/logic/PoolLogic.sol";
 import {ReserveLogic} from "../libraries/logic/ReserveLogic.sol";
-import {SupplyLogic} from "../libraries/logic/SupplyLogic.sol";
 import {MarketplaceLogic} from "../libraries/logic/MarketplaceLogic.sol";
-import {BorrowLogic} from "../libraries/logic/BorrowLogic.sol";
-import {LiquidationLogic} from "../libraries/logic/LiquidationLogic.sol";
 import {DataTypes} from "../libraries/types/DataTypes.sol";
-import {IERC20WithPermit} from "../../interfaces/IERC20WithPermit.sol";
 import {IERC20} from "../../dependencies/openzeppelin/contracts/IERC20.sol";
 import {SafeERC20} from "../../dependencies/openzeppelin/contracts/SafeERC20.sol";
-import {IWETH} from "../../misc/interfaces/IWETH.sol";
-import {ItemType} from "../../dependencies/seaport/contracts/lib/ConsiderationEnums.sol";
 import {IPoolAddressesProvider} from "../../interfaces/IPoolAddressesProvider.sol";
 import {IPoolMarketplace} from "../../interfaces/IPoolMarketplace.sol";
-import {INToken} from "../../interfaces/INToken.sol";
-import {IACLManager} from "../../interfaces/IACLManager.sol";
 import {PoolStorage} from "./PoolStorage.sol";
-import {FlashClaimLogic} from "../libraries/logic/FlashClaimLogic.sol";
 import {Address} from "../../dependencies/openzeppelin/contracts/Address.sol";
-import {IERC721Receiver} from "../../dependencies/openzeppelin/contracts/IERC721Receiver.sol";
-import {IMarketplace} from "../../interfaces/IMarketplace.sol";
-import {Errors} from "../libraries/helpers/Errors.sol";
 import {ParaReentrancyGuard} from "../libraries/paraspace-upgradeability/ParaReentrancyGuard.sol";
-import {IAuctionableERC721} from "../../interfaces/IAuctionableERC721.sol";
-import {IReserveAuctionStrategy} from "../../interfaces/IReserveAuctionStrategy.sol";
 
 /**
  * @title Pool Marketplace contract
