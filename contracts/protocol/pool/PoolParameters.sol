466eeb36dc245d3cdc21f766776cea403b1c19c6
GopherJ <alex_cj96@foxmail.com>
2023-08-15 16:24:00 +0800

    chore: revert unecessary changes
    Signed-off-by: GopherJ <alex_cj96@foxmail.com>

--- contracts/protocol/pool/PoolParameters.sol
+++ contracts/protocol/pool/PoolParameters.sol
@@ -19,7 +19,6 @@ import {IACLManager} from "../../interfaces/IACLManager.sol";
 import {PoolStorage} from "./PoolStorage.sol";
 import {FlashClaimLogic} from "../libraries/logic/FlashClaimLogic.sol";
 import {Address} from "../../dependencies/openzeppelin/contracts/Address.sol";
-import {SafeERC20} from "../../dependencies/openzeppelin/contracts/SafeERC20.sol";
 import {IERC721Receiver} from "../../dependencies/openzeppelin/contracts/IERC721Receiver.sol";
 import {IMarketplace} from "../../interfaces/IMarketplace.sol";
 import {Errors} from "../libraries/helpers/Errors.sol";
@@ -52,7 +51,6 @@ contract PoolParameters is
     uint256 internal constant POOL_REVISION = 149;
     uint256 internal constant MAX_AUCTION_HEALTH_FACTOR = 3e18;
     uint256 internal constant MIN_AUCTION_HEALTH_FACTOR = 1e18;
-    using SafeERC20 for IERC20;
 
     /**
      * @dev Only pool configurator can call functions marked by this modifier.
@@ -230,9 +228,8 @@ contract PoolParameters is
         override
         onlyPoolAdmin
     {
-        if (IERC20(token).allowance(address(this), to) == 0) {
-            IERC20(token).safeApprove(to, type(uint256).max);
-        }
+        IERC20(token).approve(to, 0);
+        IERC20(token).approve(to, type(uint256).max);
     }
 
     /// @inheritdoc IPoolParameters
