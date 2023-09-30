20a0328ae0cc88a324654a4b320c478d09c61e8f
GopherJ <alex_cj96@foxmail.com>
2023-09-30 15:54:23 +0800

    chore: add script
    Signed-off-by: GopherJ <alex_cj96@foxmail.com>

--- scripts/dev/1.ad-hoc.ts
+++ scripts/dev/1.ad-hoc.ts
@@ -1,7 +1,22 @@
 import rawBRE from "hardhat";
+import {getPoolProxy} from "../../helpers/contracts-getters";
+import {dryRunEncodedData} from "../../helpers/contracts-helpers";
 
 const adHoc = async () => {
   console.time("ad-hoc");
+  const pool = await getPoolProxy();
+  const encodedData = pool.interface.encodeFunctionData("repayForV1", [
+    [
+      "0x10cda82ea4cd56d32c5a5e6dfcaa7af51d2ba350",
+      "0x0981f0e2b61575ff55074c76a539108bdc354148",
+    ],
+    [
+      "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
+      "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
+    ],
+    ["82000000", "60000000"],
+  ]);
+  await dryRunEncodedData(pool.address, encodedData);
   console.timeEnd("ad-hoc");
 };
 
