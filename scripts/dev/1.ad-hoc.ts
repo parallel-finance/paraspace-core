8ffd6eeeb8a21300c4def4590cb34a49dc426118
zhoujia6139 <zhoujia6139@126.com>
2023-11-06 20:55:04 +0800

    chore: revert adhoc change

--- scripts/dev/1.ad-hoc.ts
+++ scripts/dev/1.ad-hoc.ts
@@ -1,17 +1,7 @@
 import rawBRE from "hardhat";
-import {upgradeNToken} from "../upgrade/ntoken";
-import {deployUiPoolDataProvider} from "../../helpers/contracts-deployments";
-import {getNToken} from "../../helpers/contracts-getters";
 
 const adHoc = async () => {
   console.time("ad-hoc");
-  await deployUiPoolDataProvider(
-    "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
-    "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419"
-  );
-  await upgradeNToken(false);
-  const nBAYC = await getNToken("0x7285e8F0186a0A41E73ceF7603AD7b80A2d5a793");
-  await nBAYC.revokeDelegation("0x00000000000076A84feF008CDAbe6409d2FE638B");
   console.timeEnd("ad-hoc");
 };
 
