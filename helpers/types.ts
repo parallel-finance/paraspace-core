8b1c2ee9bd6e7d56d3e8e7c3bd0fd63145f6020a
zhoujia6139 <zhoujia6139@126.com>
2023-12-07 15:03:34 +0800

    chore: aUSDC

--- helpers/types.ts
+++ helpers/types.ts
@@ -458,6 +458,7 @@ export interface iAssetBase<T> {
   DAI: T;
   WETH: T;
   USDC: T;
+  aUSDC: T;
   USDT: T;
   FRAX: T;
   WBTC: T;
@@ -591,6 +592,7 @@ export enum ERC20TokenContractId {
   DAI = "DAI",
   WETH = "WETH",
   USDC = "USDC",
+  aUSDC = "aUSDC",
   USDT = "USDT",
   FRAX = "FRAX",
   WBTC = "WBTC",
